95d8312ef095b080b7df569d831941f900d18493
mk-oussen <mokhtar.ouarouss.10@gmail.com>
2023-06-03 13:45:50 +0100

    commit

--- ticket/views.py
+++ ticket/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
 
 def Event_List(request):
     #context={'Events':Event.objects.all()}
