0d466ec4d67ad7ab22679ff2e63480a61c8c4e86
mk-oussen <mokhtar.ouarouss.10@gmail.com>
2023-06-03 13:30:39 +0100

    commit

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+app_name = 'ticket'
+
 urlpatterns = [
-    path('', views.index, name='/'),
+    
+    path('', views.Event_List, name='event_list'),
+    #path('contact', views.d_contact, name='d_contact'),
+   # path('contact', views.contact_form, name='f_contact'),
+   # path('signup/', views.signup, name='signup'),
+    #path('profil/', views.Profil, name='signup'),
+   # path('<slug:slug>/', views.post_detail, name='post_detail'),
+   # path('Events', views.Event_List, name='event_list')
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
-def index(request):
-    pass
+
+def Event_List(request):
+    #context={'Events':Event.objects.all()}
+    return render(request,'Events.html')
